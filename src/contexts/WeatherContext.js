ef1892b93e6b6045413af81437ab370f587b9772
TBence2002 <torokbencee.2002@gmail.com>
2024-02-15 18:18:16 +0100

    Contexts

---
+++ src/contexts/WeatherContext.js
@@ -0,0 +1,130 @@
+import { createContext, useContext, useReducer, useEffect } from "react";
+import { weatherReducer } from "../reducers/weatherReducer"; 
+import axios from "axios";
+
+const initialState = {
+    weatherList: [],
+    forecastList: [],
+    country: [],
+    userLocation: {},
+
+}
+
+const WeatherContext = createContext(initialState);
+
+export const WeatherProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(weatherReducer, initialState);
+
+    const getCurrentPosition = () => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                dispatch({
+                    type:"GET_LOCATION",
+                    payload: {
+                        location: position.coords
+                    }
+                })
+            },
+            (error) => {
+                console.error("Hiba történt a helymeghatározás során: ", error);
+                console.log(error.message);
+            }
+        )
+    }
+
+    useEffect(() => {
+        getCurrentPosition();
+    }, []);
+
+    useEffect(() => {
+        if(state.userLocation && state.userLocation.latitude && state.userLocation.longitude) {
+              
+        getAllWeatherInfoFromPosition(state.userLocation.latitude, state.userLocation.longitude);
+        getAllDailyForecastFromPosition(state.userLocation.latitude, state.userLocation.longitude);
+
+        }
+    }, [state.userLocation]);
+
+    const getAllWeatherInfoFromPosition = (latitude, longitude) => {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=9d771ee9b9c4e223095e65a820c0fb16`)
+            .then((response) => {
+                dispatch({
+                    type:"GET_WEATHER",
+                    payload: {
+                        weather: response.data
+                    }
+                })
+            })
+            .catch((error) => {
+                console.error("Hiba történt a lekérés során:", error);
+                console.log(error.message);
+            });
+    }
+
+    const getAllDailyForecastFromPosition = (latitude, longitude) => {
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=9d771ee9b9c4e223095e65a820c0fb16`)
+            .then((response) => {
+                dispatch({
+                    type:"GET_FORECAST",
+                    payload: {
+                        forecast: response.data.list
+                    }
+                })
+            })
+            .catch((error) => {
+                console.error("Hiba történt a lekérés során:", error);
+                console.log(error.message);
+            });
+    }
+
+    
+      const getAllWeatherInfoFromSearch = (search) => {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=9d771ee9b9c4e223095e65a820c0fb16`)
+            .then((response) => {
+              dispatch({
+                type: "GET_WEATHER",
+                payload: {
+                    weather: response.data
+                }
+              })
+            })
+            .catch((error) => {
+              console.error("Hiba történt a városok lekérése során:", error);
+            });
+      }
+    
+      const getAllDailyForecastFromSearch = (search) => {
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=9d771ee9b9c4e223095e65a820c0fb16`)
+            .then((response) => {
+              dispatch({
+                type: "GET_FORECAST",
+                payload: {
+                    forecast:response.data.list
+                }
+              })
+            })
+            .catch((error) => {
+              console.error("Hiba történt a városok lekérése során:", error);
+            });
+      }
+
+    const value = {
+        weatherList: state.weatherList,
+        forecastList: state.forecastList,
+        userLocation: state.userLocation,
+        country: state.country,
+        getAllWeatherInfoFromSearch,
+        getAllDailyForecastFromSearch
+    }
+
+    return (
+        <WeatherContext.Provider value={value}>
+            {children}
+        </WeatherContext.Provider>
+    )
+}
+
+export const useWeather = () => {
+    const context = useContext(WeatherContext);
+    return context;
+}
